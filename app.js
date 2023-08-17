33cfb81272d8798eee98d0f382f682b068cfbfdf
Bagas Arisandi HIdayat <70928493+bagassandih@users.noreply.github.com>
2023-08-17 18:20:08 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const cron = require('node-cron');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 const utilities = require('./utils');
+
+app.use('/', (req, res, next) => {
+  const filePath = path.join(__dirname, './data_price.txt'); // Ganti dengan nama file Anda
+  res.setHeader('Content-Type', 'text/plain'); // Set tipe konten ke text/plain
+  res.sendFile(filePath);
+});
 
 cron.schedule('0 7 * * *', async () => {
   await utilities.getPriceGold();
